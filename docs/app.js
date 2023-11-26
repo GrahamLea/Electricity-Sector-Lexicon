f341fcab878950f402866ffcc39499fbf58694cd
Graham Lea <graham@grahamlea.com>
2023-11-26 17:22:05 +1100

    Render links to other terms in definition text & use the URL hash to show a single term
    Put warning signs around links in definition text that don't resolve to defined terms
    Convert the text of a term in the URL hash into its ID
    Hide intro text if a single term is being shown
    Show an info box when a single term is showing, with a button to show all entries
    Show a warning if a term in the hash doesn't exist
    Add Bootstrap icons stylesheet
    Await all data load promises before trying to find definition in hash

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,9 @@
-const { createApp } = Vue
+const {createApp} = Vue
 
 const DATA_ROOT = "data/index.json5"
+
+const LINK_REGEX = /(\[.+?])/
+const SPACES_REGEX = / +/g
 
 function deepCopy(object) {
     return JSON.parse(JSON.stringify(object))
@@ -10,6 +13,7 @@ createApp({
     data() {
         return {
             message: 'Hello Vue!',
+            selectedTerm: null,
             definitions: []
         }
     },
@@ -25,8 +29,28 @@ createApp({
             return cats
         },
 
+        selectedDefinition() {
+            return this.definitionsById.get(this.selectedTerm)
+        },
+
+        definitionsById() {
+            let map = new Map()
+            for (let d of this.definitions) {
+                map.set(d.id, d)
+            }
+            return map
+        },
+
         definitionsByCategory() {
             let map = new Map()
+
+            if (this.selectedTerm) {
+                if (this.selectedDefinition) {
+                    map.set(this.selectedDefinition.category, [this.selectedDefinition])
+                }
+                return map
+            }
+
             for (let c of this.categories) {
                 map.set(c, [])
             }
@@ -38,54 +62,102 @@ createApp({
     },
 
     methods: {
-      async loadData(file, context) {
-          const response = await fetch(file);
-          if (!response.ok) {
-              alert(`Error loading data: ${response.statusText} (${response.status})`)
-              return
-          }
-          let text = await response.text();``
-          const data = JSON5.parse(text)
-          if (data["category"]) {
-              if (context.category) {
-                  context.category = context.category + " / " + data["category"]
-              } else {
-                  context.category = data["category"]
-              }
-          }
-          if (data["jurisdiction"]) {
-              context.jurisdiction = data["jurisdiction"]
-          }
-          if (data["tags"]) {
-              context.tags = (context.tags || []).concat(data["tags"])
-          }
+        async loadData(file, context) {
+            const response = await fetch(file);
+            if (!response.ok) {
+                alert(`Error loading data from ${file}: ${response.statusText} (${response.status})`)
+                return
+            }
+            let text = await response.text();
+            const data = JSON5.parse(text)
+            if (data["category"]) {
+                if (context.category) {
+                    context.category = context.category + " / " + data["category"]
+                } else {
+                    context.category = data["category"]
+                }
+            }
+            if (data["jurisdiction"]) {
+                context.jurisdiction = data["jurisdiction"]
+            }
+            if (data["tags"]) {
+                context.tags = (context.tags || []).concat(data["tags"])
+            }
 
-          let newDefinitions = []
-          for (let definition of data["definitions"] || []) {
-              if (!context.category) {
-                  alert(`Data error: Definition has no category: ${definition["term"]}`)
-              }
-              definition["category"] = context.category
-              definition["jurisdiction"] = context.jurisdiction
-              definition["tags"] = (definition["tags"] || []).concat(context.tags)
-              newDefinitions.push(definition)
-          }
-          this.definitions.push(...newDefinitions)
+            let newDefinitions = []
+            for (let definition of data["definitions"] || []) {
+                if (!context.category) {
+                    alert(`Data error: Context has no category while processing: ${definition["term"]}`)
+                    continue
+                }
+                definition["id"] = this.termId(definition["term"])
+                definition["category"] = context.category
+                definition["jurisdiction"] = context.jurisdiction
+                definition["tags"] = (definition["tags"] || []).concat(context.tags)
+                newDefinitions.push(definition)
+            }
+            this.definitions.push(...newDefinitions)
 
-          let directory = file.substring(0, file.lastIndexOf("/"));
-          for (let importLocation of data["include"] || []) {
-              let importFile
-              if (importLocation.endsWith("/")) {
-                  importFile = `${directory}/${importLocation}index.json5`
-              } else {
-                  importFile = `${directory}/${importLocation}.json5`
-              }
-              this.loadData(importFile, deepCopy(context))
-          }
-      }
+            const importPromises = []
+            let directory = file.substring(0, file.lastIndexOf("/"));
+            for (let importLocation of data["include"] || []) {
+                let importFile
+                if (importLocation.endsWith("/")) {
+                    importFile = `${directory}/${importLocation}index.json5`
+                } else {
+                    importFile = `${directory}/${importLocation}.json5`
+                }
+                importPromises.push(this.loadData(importFile, deepCopy(context)))
+            }
+            await Promise.allSettled(importPromises)
+        },
+
+        textSections(text) {
+            return text.split(LINK_REGEX)
+        },
+
+        termId(term) {
+            return term.toLowerCase().replaceAll(SPACES_REGEX, "-")
+        },
+
+        isLink(text) {
+            return text.match(LINK_REGEX)?.[0] === text
+        },
+
+        linkTagFor(text) {
+            const term = text.slice(1, -1)
+            if (this.definitionsById.has(this.termId(term))) {
+                return `<a href="#${this.termId(term)}" onclick="">${term}</a>`
+            } else {
+                return `<i class="bi bi-exclamation-diamond-fill"></i>️ [${term}] <i class="bi bi-exclamation-diamond-fill"></i>`
+            }
+        },
+
+        clearSelectedTerm() {
+            this.selectedTerm = undefined
+            // Note: The below doesn't trigger a 'hashchange' event
+            history.pushState('', document.title, window.location.pathname + window.location.search)
+        },
+
+        updateFromHash() {
+            const hash = window.location.hash;
+            if (hash && hash !== "" && hash !== "#") {
+                let term = hash.startsWith("#") ? hash.slice(1) : hash
+                term = term.replaceAll("%20", " ")
+                this.selectedTerm = term
+                if (!this.selectedDefinition && this.definitionsById.has(this.termId(term))) {
+                    window.location.hash = this.termId(term)
+                }
+                window.scrollTo(0,0)
+            } else {
+                this.selectedTerm = undefined
+            }
+        }
     },
 
-    mounted() {
-        this.loadData(DATA_ROOT, {})
+    async mounted() {
+        await this.loadData(DATA_ROOT, {})
+        addEventListener("hashchange", this.updateFromHash);
+        this.updateFromHash();
     }
 }).mount('#app')
